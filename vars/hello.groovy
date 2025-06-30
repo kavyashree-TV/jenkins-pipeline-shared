13c524a73516b57347a0bf156c3e3aee6988ffe8
kavyashree-TV <kavyashree8240@gmail.com>
2025-06-30 15:04:45 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,1 +1,13 @@
 
+@Library('jenkins-shared-lib') _  // Load the shared library by name
+
+pipeline {
+    agent any
+    stages {
+        stage('Greeting') {
+            steps {
+                hello('Jenkins User')  // Calls hello.groovy from vars/
+            }
+        }
+    }
+}
